3ad3899b8aacd9c4811fc157d1a2567752a97dec
Divya Anand <divue2003@gmail.com>
2024-11-25 11:18:51 +0530

    utilization code

---
+++ 3_shell_on_pipeline/disk_utilization.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+usage=$(df / | grep / | awk '{print $5}' | sed 's/%//')
+
+if [ "$usage" -gt 80 ]; then
+	echo "DISK UTILIZATION IS AT ${usage}%!!" | mail -s "YOUR DISK IS UTILISING MORE THAN 80%" divue2003@gmail.com
+else
+	echo " Disk Utilization under control "
+fi
